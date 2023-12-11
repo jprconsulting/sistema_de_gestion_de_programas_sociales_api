ef743604f0bc34175d9e949de0fad4733fdc72ae
Gabriel <g27r02@gmail.com>
2023-12-11 11:56:52 -0600

    usuario

--- beneficiarios_dif_api/DTOs/UsuarioInsertDTO.cs
+++ beneficiarios_dif_api/DTOs/UsuarioInsertDTO.cs
@@ -2,7 +2,7 @@
 {
     public class UsuarioInsertDTO
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Nombre { get; set; }
         public string ApellidoPaterno { get; set; }
         public string ApellidoMaterno { get; set; }
