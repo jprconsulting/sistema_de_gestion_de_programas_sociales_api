351ef5422607ba37ef9e2645b4649eed8796c9e6
Gabriel <g27r02@gmail.com>
2023-12-20 09:44:59 -0600

    EVIDENCIA

--- beneficiarios_dif_api/DTOs/EvidenciaDTO.cs
+++ beneficiarios_dif_api/DTOs/EvidenciaDTO.cs
@@ -2,7 +2,7 @@
 {
     public class EvidenciaDTO
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public int BeneficiarioId { get; set; }
         public string Descripcion { get; set; }
         public string ImagenBase64 { get; set; }
